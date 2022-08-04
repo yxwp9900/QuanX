9ffad1fef9cc16e9cc9837056714c74630ea0718
paul <yxwp9900@gmail.com>
2022-08-04 18:23:09 +0800

    caiyun weather

--- Scripts/wtrs.js
+++
@@ -1,4 +0,0 @@
-// ^https:\/\/api\.interpreter\.caiyunai\.com\/v1\/page\/auth
-var tp = new Date().getTime()
-var body = $response.body.replace(/browserid":\".+\"/g, 'browserid":"{}'.format(tp));
-$done({ body });
